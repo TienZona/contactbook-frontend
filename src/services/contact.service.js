dde2c9454b60a22dce0cbea2d435b316cab2eebc
TienZona <tienzona001@gmail.com>
2022-10-27 23:25:26 +0700

    Hoan thien du an

--- src/services/contact.service.js
+++ src/services/contact.service.js
@@ -10,7 +10,7 @@ class ContactService {
     }
 
     async create(data){
-        return (await this.api.post('/'), data).data;
+        return (await this.api.post('/', data)).data;
     }
 
     async deleteAll(){
