632bb64580b0ec06b53ddd1521d1cd37d87291f5
TienZona <tienzona001@gmail.com>
2022-11-01 15:34:45 +0700

    update final

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,44 @@
+<template>
+    <div v-if="contact" class="page">
+        <h4>Hiệu chỉnh Liên hệ</h4>
+        <ContactForm
+            :contact="contact"
+            @submit:contact="addContact"
+        />
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+    import ContactForm from "@/components/ContactForm.vue";
+    import ContactService from "@/services/contact.service";
+    export default {
+        components: {
+            ContactForm,
+        },
+        props: {
+            id: { type: String, required: true },
+        },
+        data() {
+            return {
+                contact: {
+                        name: "",
+                        email: "",
+                        address: "",
+                        phone: ""
+                    },
+                message: "",
+            };
+        },
+        methods: {
+            async addContact(data) {
+                console.log(data)
+                try {
+                    await ContactService.create(data);
+                    this.message = "Thêm mới liên hệ thành công.";
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
+    };
+</script>
